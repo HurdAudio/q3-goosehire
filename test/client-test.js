e2edc284b0ebff81baf43742dd35d9c0ffd69b45
Devin Hurd <hurdaudio@gmail.com>
2017-01-21 15:37:42 -0700

    test for skillsets and users complete

--- test/client-test.js
+++ test/client-test.js
@@ -24,6 +24,8 @@ suite('Verify basic functionality of GooseHire API', () => {
 
 });
 
+// TEST FOR SEARCHES
+
 suite('verify basic functionality of searches', function(){
 
     test('GET /index.html/searches - returns all searches in users account', (done) => {
@@ -101,6 +103,60 @@ suite('verify basic functionality of skillsets', function(){
         .expect('Content-Type', /json/)
         .expect(200, done);
     });
+
+    test('GET /index/skillsets by id  - returns json of single record.', (done) => {
+      request(app)
+        .get('/skillsets/1')
+        .expect('Content-Type', /json/)
+        .expect(200, {
+          1: ['horse rangling', 'clowning', 'magic tricks', 'exorcism'],
+          2: ['javascript', 'angular', 'writing tests']
+        }, done);
+    });
+
+    test('POST /index/skillsets - create new skillsets.', (done) => {
+        request(app)
+          .post('/skillsets')
+          .set('Accept', 'application/json')
+          .send({
+            1: ['dare devil', 'comedian', 'Daytime Talkshow Host'],
+            2: ['scupa diver', 'elvis impersonator', 'jesus impersonator']
+          })
+          .expect('Content-Type', /json/)
+          .expect(200, {
+            1: ['dare devil', 'comedian', 'Daytime Talkshow Host'],
+            2: ['scupa diver', 'elvis impersonator', 'jesus impersonator']
+          },done);
+
+    });
+
+    test('PATCH /index/skillsets - revises skillsets.', (done) => {
+      request(app)
+        .patch('/skillsets/1')
+        .set('Accept', 'application/json')
+        .send({
+          1: ['Hobbit', 'Wizard', 'Elf']
+        })
+        .expect('Content-Type', /json/)
+        .expect(200, {
+          1: ['Hobbit', 'Wizard', 'Elf'],
+          2: ['javascript', 'angular', 'writing tests']
+        },done);
+
+    });
+
+    test('DELETE /index/skillsets - delete skillset item.', (done) => {
+      request(app)
+        .del('/skillsets/2')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, {
+          1: ['dare devil', 'comedian', 'Daytime Talkshow Host'],
+          2: ['scupa diver', 'elvis impersonator', 'jesus impersonator']
+        },done);
+
+
+    });
   });
 
 suite('verify basic functionality of users', function(){
@@ -111,5 +167,60 @@ suite('verify basic functionality of users', function(){
         .get('/users')
         .expect('Content-Type', /json/)
         .expect(200, done);
+    });
+
+    test('GET /index/users by id  - returns json of single record.', (done) => {
+      request(app)
+        .get('/users/1')
+        .expect('Content-Type', /json/)
+        .expect(200, {
+          name: "Devin Hurd",
+          email: "hurd@gmail.com"
+        }, done);
+    });
+
+    test('POST /index/users - create new users.', (done) => {
+        request(app)
+          .post('/users')
+          .set('Accept', 'application/json')
+          .send({
+            name: 'Jason Goldfarb',
+            email: 'farb@gmail.com'
+
+          })
+          .expect('Content-Type', /json/)
+          .expect(200, {
+            name: 'Jason Goldfarb',
+            email: 'farb@gmail.com'
+          },done);
+
+    });
+
+    test('PATCH /index/users - revises users.', (done) => {
+      request(app)
+        .patch('/users/1')
+        .set('Accept', 'application/json')
+        .send({
+          email: 'george@gmail.com'
+        })
+        .expect('Content-Type', /json/)
+        .expect(200, {
+          name: 'Devin Hurd',
+          email: 'george@gmail.com'
+        },done);
+
+    });
+
+    test('DELETE /index/users - delete users item.', (done) => {
+      request(app)
+        .del('/users/2')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, {
+          name: 'Jason Goldfarb',
+          email: 'farb@gmail.com'
+        },done);
+
+
     });
   });
