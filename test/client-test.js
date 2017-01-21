7a6ab35d449df50426abe5fee916a319ba89af28
Devin Hurd <hurdaudio@gmail.com>
2017-01-21 15:09:21 -0700

    test for for search complete

--- test/client-test.js
+++ test/client-test.js
@@ -26,13 +26,70 @@ suite('Verify basic functionality of GooseHire API', () => {
 
 suite('verify basic functionality of searches', function(){
 
-    test('GET /index.html', (done) => {
+    test('GET /index.html/searches - returns all searches in users account', (done) => {
 
       request(app)
         .get('/searches')
         .expect('Content-Type', /json/)
         .expect(200, done);
     });
+
+    test('GET /index/searches by id  - returns json of single record.', (done) => {
+      request(app)
+        .get('/searches/1')
+        .expect('Content-Type', /json/)
+        .expect(200, {
+          title: 'Fluffer',
+          location: 'San Antonio, TX'
+        }, done);
+    });
+
+    test('POST /index/searches - create new search.', (done) => {
+        request(app)
+          .post('/searches')
+          .set('Accept', 'application/json')
+          .send({
+            title: 'Fluffer',
+            location: 'Seatle, WA'
+          })
+          .expect('Content-Type', /json/)
+          .expect(200, {
+            title: 'Fluffer',
+            location: 'Seatle, WA'
+          },done);
+
+    });
+
+    test('PATCH /index/searches - revises search.', (done) => {
+      request(app)
+        .patch('/searches/1')
+        .set('Accept', 'application/json')
+        .send({
+          title: 'Horse Whisperer'
+        })
+        .expect('Content-Type', /json/)
+        .expect(200, {
+          title: 'Horse Whisperer',
+          location: 'San Antonio, TX'
+        },done);
+
+    });
+
+    test('DELETE /index/searches - delete search item.', (done) => {
+      request(app)
+        .del('/search/2')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, {
+          title: 'Horse Whisperer',
+          location: 'San Antonio, TX'
+        },done);
+
+
+    });
+
+
+
   });
 
 suite('verify basic functionality of skillsets', function(){
