c9dd0a53b4f45416fadaec00091b21e523dd4cfd
malilasage <malilaclearwater@gmail.com>
2017-01-21 14:35:19 -0700

    added skills and search schema

---
+++ src/skills_schema.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const SkillsSchema = new Schema({
+  skillSet: [String],
+  createdAt: { type: Date, default: Date.now}
+});
---
+++ src/users.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+const SkillsSchema = require('./skills_schema');
+const SearchSchema = require('./search_schema');
+const Schema = mongoose.Schema;
