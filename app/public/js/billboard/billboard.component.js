2da97e4a3ff6549bfd1310fd892dab7fa49c7eb6
Brad Efting <bradefting@gmail.com>
2017-01-22 12:29:50 -0700

    save before merge

--- app/public/js/billboard/billboard.component.js
+++ app/public/js/billboard/billboard.component.js
@@ -10,6 +10,44 @@
 
     function BillboardController(){
       const vm = this;
+      console.log(vm);
+
+      vm.slides = [
+           {image: '/images/GooseHire.png', description: 'Image 00'},
+           {image: '/images/canada-goose.jpg', description: 'Image 01'}
+       ];
+
+      //  increment images
+      vm.currentIndex = 0;
+
+      // vm.index = vm.index;
+
+      // vm.setCurrentSlideIndex = function (index) {
+      //   console.log(index, "setCurrent");
+      //   vm.currentIndex = index;
+      // };
+      //
+      // vm.isCurrentSlideIndex = function (index) {
+      //   console.log(index, "isCurrent");
+      //   return vm.currentIndex === index;
+      // };
+
+
+        vm.setCurrentSlideIndex = function (index) {
+            vm.currentIndex = index;
+        };
+
+        vm.isCurrentSlideIndex = function (index) {
+            return vm.currentIndex === index;
+        };
+
+        vm.prevSlide = function () {
+            vm.currentIndex = (vm.currentIndex < vm.slides.length - 1) ? ++vm.currentIndex : 0;
+        };
+        
+        vm.nextSlide = function () {
+            vm.currentIndex = (vm.currentIndex > 0) ? --vm.currentIndex : vm.slides.length - 1;
+        };
 
     }
 
