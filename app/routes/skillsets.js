d09381bfd2260fbf0c652e8df09c55b29ca19c26
Mehri Russo <mehrimova@gmail.com>
2017-01-23 11:37:09 -0700

    starting skillsets route

--- app/routes/skillsets.js
+++ app/routes/skillsets.js
@@ -2,8 +2,32 @@
 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const User = require('../src/users');
+mongoose.Promise = require('bluebird');
 
+router.get('/', (req, res) => {
+  User.findOne({ id: _id }, 'skillsets', function (err, person) {
+    if(err) { throw err; }
+    else {
+      res.send(data);
+    }
+  });
+});
 
+  // User.find(err, data) => {
+  //   if(err) { throw err; }
+  //   else {
+  //     res.send(data);
+  //   }
+  // }
 
+router.get('/skillsets/:id', (req, res) => {
 
-module.exports = router
+});
+
+router.post('/users', (req, res) => {
+
+});
+
+module.exports = router;
