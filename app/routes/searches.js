b7071023bbc24f86721620d92ee8ae71ad48228e
malilasage <malilaclearwater@gmail.com>
2017-01-24 11:38:08 -0700

    get skills works

--- app/routes/searches.js
+++ app/routes/searches.js
@@ -2,7 +2,44 @@
 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const User = require('../src/users');
+mongoose.Promise = require('bluebird');
 
+//gets all of a users searches
+router.get('/:id', (req, res) => {
+  User.findById(req.params.id, (err, data) => {
+    if(err) { throw err; }
+    else {
+      res.send(data.searches);
+    }
+  })
+});
+
+//gets one of a users search, by userid and search id
+router.get('/:userid/:id', (req, res) => {
+  User.find({'id': req.params.userid, $where: this.searches._id === req.params.id}).exec( (err, data) => {
+    if(err) { throw err; }
+    else {
+      res.send(data);
+    }
+  })
+});
+
+//posts new search to users searches
+router.post('', (req, res) => {
+
+});
+
+//updates one of a users searches
+router.patch('', (req, res) => {
+
+});
+
+// deletes a users search
+router.delete('', (req, res) => {
+
+});
 
 
 
--- app/routes/skillsets.js
+++ app/routes/skillsets.js
@@ -2,8 +2,48 @@
 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const User = require('../src/users');
+mongoose.Promise = require('bluebird');
 
 
+router.get('/:id', (req, res) => {
+  User.findById(req.params.id, (err, data) => {
+    if(err) { throw err; }
+    else {
+      res.send(data.skills);
+    }
+  })
+});
+
+router.post('/', (req, res) => {
+  User.create({
+    username: req.body.username,
+    hashedPassword: req.body.hashedPassword
+  }, (err, data) => {
+    if(err) {throw err};
+    res.send(data);
+  })
+});
+
+router.patch('/:id', (req, res) => {
+  User.findByIdAndUpdate(req.params.id,
+    { $set: req.body }, { new: true },
+    function (err, data) {
+      if (err) throw err;
+      res.send(data);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  User.findById(req.params.id, (err, data) => {
+    if(err) throw err;
+    data.remove((err, data) => {
+      if(err) throw err;
+      res.send(data);
+    })
+  })
+});
 
 
-module.exports = router
+module.exports = router;
