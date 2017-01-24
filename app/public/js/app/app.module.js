a09b1462c9b625cccb4af25f211b369e2071d3d6
Brad Efting <bradefting@gmail.com>
2017-01-23 21:59:50 -0700

    initial design set up

--- app/public/js/app/app.module.js
+++ app/public/js/app/app.module.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
 
-  angular.module("app", ['ui.router', 'ngAnimate']);
+  angular.module("app", ['ui.router']);
 
 }());
--- app/public/js/billboard/billboard.component.js
+++ app/public/js/billboard/billboard.component.js
@@ -13,38 +13,14 @@
       console.log(vm);
 
       vm.slides = [
-           {image: '/images/GooseHire.png', description: 'Image 00'},
-           {image: '/images/canada-goose.jpg', description: 'Image 01'}
-       ];
+        {image: 'images/img00.jpg', description: 'Image 00'},
+        {image: 'images/img01.jpg', description: 'Image 01'},
+        {image: 'images/img02.jpg', description: 'Image 02'}
+      ];
 
-      //  increment images
-      vm.currentIndex = 0;
-      var count = 0;
 
-      vm.setCurrentSlideIndex = function (index) {
-          console.log(index, "setCurrent");
-          count++;
-          console.log(count);
-          vm.currentIndex = index;
-      };
-
-      vm.isCurrentSlideIndex = function (index) {
-        console.log(index, "isCurrent");
-        count++;
-        console.log(count);
-          return vm.currentIndex === index;
-      };
-
-      vm.prevSlide = function () {
-        console.log("prevSLide");
-          vm.currentIndex = (vm.currentIndex < vm.slides.length - 1) ? ++vm.currentIndex : 0;
-      };
-
-      vm.nextSlide = function () {
-        console.log("nextSLide");
-          vm.currentIndex = (vm.currentIndex > 0) ? --vm.currentIndex : vm.slides.length - 1;
-      };
 
     }
+
 
 }());
