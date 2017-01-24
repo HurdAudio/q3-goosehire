e199a9caca52cfacd87758bbe6b0acfce0f07e9b
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2017-01-24 12:47:06 -0700

    get on searches works

--- app/routes/searches.js
+++ app/routes/searches.js
@@ -2,6 +2,29 @@
 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const User = require('../src/users');
+mongoose.Promise = require('bluebird');
+
+router.get('/:id', (req, res) => {
+  User.findById(req.params.id, (err, data) => {
+    if(err) { throw err; }
+    else{
+      res.send(data);
+    }
+  })
+});
+
+router.post('/', (req, res) => {
+  User.create({
+    search:req.body.searches,
+    hashedPassword: req.body.hashedPassword
+  }, (err,data) => {
+    if(err) {throw err};
+    res.send(data.searches);
+  })
+  });
+
 
 
 
