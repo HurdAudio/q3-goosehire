5d99f178e667b502c80670f2f39b4ed7fde66a8f
malilasage <malilaclearwater@gmail.com>
2017-01-25 11:43:50 -0700

    added functions to login controller

--- app/public/js/login/login.component.js
+++ app/public/js/login/login.component.js
@@ -7,9 +7,28 @@
       templateUrl: '/js/login/login.template.html'
     });
 
-    function LoginController(){
+    LoginController.$inject = ['$http', '$state', '$stateParams'];
+
+    function LoginController($http, $state, $stateParams){
       const vm = this;
 
+      vm.$onInit() = onInit;
+      vm.login = login;
+      vm.newUser = newUser;
+
+      function onInit() {
+
+      }
+
+      function login() {
+
+      }
+
+      function newUser() {
+        $http.post('/users', vm.user).then((res) => {
+          console.log(res);
+        })
+      }
     }
 
 }());
