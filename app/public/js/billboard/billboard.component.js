51dfd355fa86ecb107aaf871c6f3c222a9a20c9f
Mehri Russo <mehrimova@gmail.com>
2017-01-25 15:03:17 -0700

    fixed nav bar and slideshow

--- app/public/js/billboard/billboard.component.js
+++ app/public/js/billboard/billboard.component.js
@@ -12,26 +12,25 @@
       console.log(vm);
 
       vm.slides = [];
-      vm.myInterval = 4000;
+      vm.myInterval = 8000;
       vm.noWrapSlides = false;
       vm.active = 0;
 
     vm.$onInit = function() {
       vm.slides = [{
-        image: '../images/img00.jpg',
-        id: 0,
-        text: 'Add your skills and desired location'
+        image: '../images/banner01.jpg',
+        id: 0
       },
       {
-        image: '../images/img00.jpg',
-        id: 1,
-        text: 'See jobs in your area'
+        image: '../images/banner02.jpg',
+        id: 1
       },
-      {
-        image: '../images/img00.jpg',
-        id: 2,
-        text: 'Modify your skills to match desired job skills'
-      }];
+      // {
+      //   image: '../images/img00.jpg',
+      //   id: 2,
+      //   text: 'Modify your skills to match desired job skills'
+      // }
+    ];
      };
 
     }
