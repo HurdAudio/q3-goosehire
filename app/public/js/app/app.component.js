420f06d3428bd29852f9ab482838b7bc92bf4b60
Brad Efting <bradefting@gmail.com>
2017-01-26 14:28:07 -0700

    save before master pull

--- app/public/js/app/app.component.js
+++ app/public/js/app/app.component.js
@@ -11,21 +11,6 @@
   function AppController($http) {
     const vm = this;
 
-    vm.loginLinkedIn = function(){
-
-      $http.get('./auth/linkedin').then(function (response) {
-        // vm.things = response.data
-        console.log('app.component', response);
-        console.log('logged in');
-      })
-
-
-    }
-
-    vm.logoutLinkedIn = function(){
-      console.log('logout');
-    }
-
     vm.$onInit = function(){
 
 
