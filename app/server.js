b6dc0d5448debd19b004b06f0c7772476eee18cc
CHANSE HORTON <chansehorton@yahoo.com>
2017-01-26 09:02:12 -0700

    changed indeed call to pull from .env

--- app/server.js
+++ app/server.js
@@ -51,7 +51,7 @@ app.get('/indeed', (req, res) => {
 
   //TODO: Do we need to get the useragent dynamically from the browser for the search string below? -- CDH
 
-  const newUrl = `http://api.indeed.com/ads/apisearch?publisher=331559334344654&q=${searchInfo.skills}&l=${searchInfo.location}&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=localhost:3000&useragent=Mozilla%2F5.0+(Macintosh%3B+Intel+Mac+OS+X+10_11_6)+AppleWebKit%2F537.36+(KHTML%2C+like+Gecko)+Chrome%2F55.0.2883.95+Safari%2F537.36&v=2&format=json`;
+  const newUrl = `http://api.indeed.com/ads/apisearch?publisher=${process.ENV.INDEED_KEY}&q=${searchInfo.skills}&l=${searchInfo.location}&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=localhost:3000&useragent=Mozilla%2F5.0+(Macintosh%3B+Intel+Mac+OS+X+10_11_6)+AppleWebKit%2F537.36+(KHTML%2C+like+Gecko)+Chrome%2F55.0.2883.95+Safari%2F537.36&v=2&format=json`;
 
   return request(newUrl).pipe(res);
 });
